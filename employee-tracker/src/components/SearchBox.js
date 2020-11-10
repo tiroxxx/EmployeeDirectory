53a2b7a2f2ed685a6af90663d4b956374e1e6cbb
tiroxxx <christianmarquez41@gmail.com>
2020-11-09 21:52:47 -0500

    Updates

--- employee-tracker/src/components/SearchBox.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function SearchBox({ handleInputChange }) {
-    return (
-        <input onChange={handleInputChange} className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-    )
-}
-
-export default SearchBox;
